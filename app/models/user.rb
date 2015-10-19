58f0363f4e30ef7c7d2ee72b58cae9a40ddcf0fc
Alexander Fong <alpfong@gmail.com>
2015-10-18 17:59:24 -0700

    Autogenerated model and basic routing for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
@@ -35,7 +33,4 @@ class User < ActiveRecord::Base
       where(conditions.to_h).first
     end
   end
-
-
-
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,5 +262,4 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
   config.authentication_keys = [ :login ]
-
 end
