cde83aebf04b8b443c4769887f707080c238ab22
Chris Konstad <chriskon149@gmail.com>
2015-10-24 12:54:54 -0700

    Fixed failing TravisCI test

--- test/lib/analyzer_test.rb
+++ test/lib/analyzer_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 class AnalyzerTest < ActiveSupport::TestCase
   test 'Run analyzer on small string' do
     a = Analyzer.new
-    assert a.process('hello world').to_json.eql?
-    '{"text":"hello world","probability":0.7627410869620818,"sentiment":":)"}'
+    assert a.process('hello world').to_json.eql? '{"text":"hello world",'\
+      '"probability":0.7627410869620818,"sentiment":":)"}'
   end
 end
