8cfcccc4b458711a51acc54e90e81952eea8c93c
Chris Konstad <chriskon149@gmail.com>
2015-10-25 02:50:13 -0700

    Revert "Twitter"

--- app/controllers/market_chirp_controller.rb
+++ app/controllers/market_chirp_controller.rb
@@ -1,11 +1,4 @@
 class MarketChirpController < ApplicationController
   def index
-    if !params[:search].blank?
-      @searching = true
-      @query = params[:search].to_s
-      num = 100
-      # omit retweets with '-rt', result type: mixed for popular and recent
-      @tweets = $twitter.search(@query + ' -rt', result_type: 'mixed', count: 20).take(num)
-    end
   end
 end
