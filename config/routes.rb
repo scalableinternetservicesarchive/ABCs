52adfda22a563e065f78db9f4c930835d8a21c1a
Alexander Fong <alpfong@gmail.com>
2015-10-25 02:14:08 -0700

    Testing yahoofinance gem to get stock quotes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get 'api/symbols/json' => 'api#symjson'
 
   get 'sentiment' => 'sentiment#check'
+  get 'stocks' => 'yahoo_finance#stocks'
+  post 'stocks' => 'yahoo_finance#get_data'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
