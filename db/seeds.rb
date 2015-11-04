c4853119d66484ebd7dac98aa780bbb6a3dcd5eb
Chris Konstad <chriskon149@gmail.com>
2015-11-03 16:45:43 -0800

    Fixed bug with top row, fixed linter errors
    Sorry for making one commit, but fixed bug where top row was skipped
    over (only 3091 companies imported instead of 3092).

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,23 @@
 require 'csv'
 
 # Load up the list of all companies and their name, symbol, sector, industry
-# Old header "Symbol","Name","LastSale","MarketCap","IPOyear","Sector","industry","Summary Quote",
+# Old header "Symbol","Name","LastSale","MarketCap","IPOyear","Sector",
+#            "industry","Summary Quote",
 # Column index mapping:
-#  0: Symbol 
-#  1: Name 
-#  2: LastSale 
-#  3: MarketCap 
-#  4: IPOyear 
-#  5: Sector 
-#  6: industry 
-#  7: Summary Quote 
-CSV.foreach('db/csv/companies.csv',
-            headers: true) do |row|
-              c = Company.new
-              c.industry = row[6]
-              c.name = row[1]
-              c.sector = row[5]
-              c.symbol = row[0]
-              c.save
-            end
+#  0: Symbol
+#  1: Name
+#  2: LastSale
+#  3: MarketCap
+#  4: IPOyear
+#  5: Sector
+#  6: industry
+#  7: Summary Quote
+CSV.foreach('db/csv/companies.csv') do |row|
+  c = Company.new
+  c.industry = row[6]
+  c.name = row[1]
+  c.sector = row[5]
+  c.symbol = row[0]
+  c.save
+end
 puts "There are now #{Company.count} rows in the Company table"
