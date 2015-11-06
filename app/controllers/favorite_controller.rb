415c5c2ee365f0774e50319ec6e7eef7c7f4a33f
Sakibs <sakib@Sakibs-MacBook-Pro.local>
2015-11-05 18:06:09 -0800

    Implement buttons to add and remove favorit using ajax

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,4 +1,6 @@
 class FavoriteController < ApplicationController
+  respond_to :json, :js, :html
+
   def favorite
     return unless current_user
     # get IDs of companies that are currently favorited
@@ -6,6 +8,7 @@ class FavoriteController < ApplicationController
     # Set the current user companies to the active ones
     @user_companies = current_user.companys.where(id: user_favorite_active)
 
+    @tickers = ticker_json(Company.all)
 
     return unless params['symbol']
     query = params['symbol'].upcase
@@ -20,10 +23,16 @@ class FavoriteController < ApplicationController
 
     if !current_user.companys.exists?(@company_id)
       current_user.companys << c
+      @message = 'creating new entry'
     else
       current_user.favorite_companys.find_by_company_id(@company_id).update(:active => true)
       @message = 'already exists, enabling favorite again'
     end
+
+    respond_to do |format|
+      format.json {render :json => {action: 'create', company: c}}
+    end
+    # respond_with(@company_id)
   end
 
 
@@ -31,6 +40,11 @@ class FavoriteController < ApplicationController
     @company_id = params['company_id']
     current_user.favorite_companys.find_by_company_id(@company_id).update(:active => false)
     @message = 'Removed from favorites'
+    # respond_with(@company_id)
+
+    respond_to do |format|
+      format.json {render :json => {action: 'remove', company_id: @company_id}}
+    end
   end
 
 
