6d2256021a127ddb92e74e7ce21af2645801e9b6
Alexander Fong <alpfong@gmail.com>
2015-11-06 12:13:53 -0800

    Removed caching to focus on current stock data
    Added typeahead and styling from sentiment page to get the same
    feel as sentiment searching. Also fixed the bug where it would be
    displaying the type of finance object we got from the query.

--- app/lib/yahoo_finance.rb
+++ app/lib/yahoo_finance.rb
@@ -6,22 +6,18 @@ class Finance
   # Looks up symbol in the finance cache. Historical data is current for up to 
   # a day in the cache and current data for 15 minutes. After that new data 
   # must be fetched
-  def self.get_quotes(symbol, hist)
+  def self.get_quotes(symbol, is_hist)
     symbol.upcase!
     if Company.find_by(symbol: symbol).nil?
       fail(ArgumentError,
            "#{symbol} is an invalid symbol! Not found in company database")
     end
 
-    #cached_result = get_cached_data(symbol, hist)
-    if hist 
-      return fetch_finance_hist(symbol)# unless cached_result
+    if is_hist 
+      fetch_finance_hist(symbol)
     else
-      return fetch_finance_curr(symbol)# unless cached_result
+      fetch_finance_curr(symbol)
     end
-
-    { quotes: cached_result.curr_data,
-      hist: cached_result.hist_data }
   end
 
   class << self
@@ -43,56 +39,15 @@ class Finance
       hist = YahooFinance::get_historical_quotes_days(symbol, days_past)
       histJSON = hist.map { |e| {:id => e}}.to_json
 
-      # Caching the historical data
-      #cache(histJSON, symbol, true)
-      
       { hist: histJSON }
-
     end
 
     # Download and cache the current quote for target stock
     def fetch_finance_curr(symbol)
       # Get new data
       quote = YahooFinance::get_standard_quotes(symbol)
-      #cache(quote, symbol, false)
 
       { quotes: quote }
-
-    end
-
-    # Stores the results in the DB
-    def cache(data, symbol, historical)
-      # :status is an enum which identifies historical (0) or current(1) data
-      data_type = historical ? :hist_data : :curr_data
-      time_type = historical ? :hist_when : :curr_when
-      #if FinanceCache.exists?(company_id: Company.find_by(symbol: symbol).id)
-      #  update_cache = 
-      #    FinanceCache.where(company_id: Company.find_by(symbol: symbol).id)
-      #  update_cache = update_attributes(data_type => data,
-      #                                   time_type => Time.now)
-      #else
-        yhoo_cache = 
-          FinanceCache.new(data_type => data,
-                           time_type => Time.now,
-                           company_id: Company.find_by(symbol: symbol).id)
-        yhoo_cache.save
-      #end
-    end
-
-    # Checks cache for stored stock data
-    def get_cached_data(symbol, historical)
-      company = Company.find_by(symbol: symbol)
-      type = historical ? 0 : 1
-      cached = FinanceCache.where(company_id: company.id, :status => type)
-
-      # Only keep historical data for a day, and current for 15 minutes
-      exp_period = historical ? 1.day : 15.min
-      if cached.created_at < Time.zone.now - exp_period
-        cached.destory
-        cached = nil
-      end
-    rescue
-      return nil
     end
   end
 end
