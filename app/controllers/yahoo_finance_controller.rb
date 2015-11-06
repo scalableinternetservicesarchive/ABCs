2824aa16752d7c8809119a983869d658f62648ea
Alexander Fong <alpfong@gmail.com>
2015-11-05 19:34:17 -0800

    Added caching of historical and current data
    Moved some of yahoo finance code into lib file so its easier to
    use elsewhere. Historical data as old as 100 years are queried and
    don't expire for a day. Current data expires after 15 minutes.
    
    Currently there is an issue with inserting entries into the database
    where for some reason, mysql thinks there is an entry with the same
    company_id key, even if inserting into a blank table.

--- app/controllers/yahoo_finance_controller.rb
+++ app/controllers/yahoo_finance_controller.rb
@@ -1,54 +1,27 @@
-require 'yahoofinance'
+require 'yahoo_finance'
 
 class YahooFinanceController < ApplicationController
-  def get_data
-    ticker = params["stock"].upcase
+  def stocks
+    # Get list of tickers for autocomplete
+    @tickers = ticker_json(Company.all)
+    return unless params['symbol']
 
-    #quote_type = YahooFinance::StandardQuote
-    @quotes = YahooFinance::get_standard_quotes(ticker)
-
-    # To get rid of the YahooFinance::StandardQuote thing at the top:
-    #   try changing the stocks.html.erb to the following:
-    #   if @ticker
-    #     YahooFinance::get_quotes(YahooFinance::StandardQuote, ticker) do |qt|
-    #       qt
-    #     end
-    #   end
-
-    # Getting the historical quote data as a raw array.
-    # The elements of the array are:
-    #   [0] - Date
-    #   [1] - Open
-    #   [2] - High
-    #   [3] - Low
-    #   [4] - Close
-    #   [5] - Volume
-    #   [6] - Adjusted Close
-
-    # Below are 2 methods to get the historical data
-    #
-    #
-    # The first downloads up to 100 years worth of quotes as raw array
-    # If using raw array, can use row.join on each obj to display
-    @hist = YahooFinance::get_historical_quotes_days(ticker, 30)
-    @histJSON = @hist.map { |e| {:id => e}}.to_json
-
-    # The second way
-    # Downloading as YahooFinance::HistoricalQuote object
-    # If using this method, change the following in stocks.html.erb
-    #   for each object, hq: hq.symbol, hq.date, hq.open, hq.high, hq.low
-    #                        hq.close, hq.volume, hq.adjClose
-    #
-    # ****to_json has some weird output when using this method*****
-    #
-    #@hist = YahooFinance::get_HistoricalQuotes_days(ticker, 36500)
-    #@histJSON = @hist.map { |e| {:id => e}}.to_json
-
-    render 'stocks'
+    # get cached symbol if possible
+    @symbol = params['symbol']
+    @quotes = Finance.get_quotes(@symbol, false)[:quotes]
+    @hist = Finance.get_quotes(@symbol, true)[:hist]
+  rescue => e
+    puts e.message
+    render file: 'public/404.html', status: :not_found, layout: false
   end
 
   private
-  def stock_ticker
+  def ticker_json(companies)
+    list = companies.map(&:symbol)
+    list.to_json
+  end
+
+  def finance_params
     params.require(:stock)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030213319) do
+ActiveRecord::Schema.define(version: 20151104013300) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "symbol",     limit: 5
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20151030213319) do
 
   add_index "companies", ["symbol"], name: "index_companies_on_symbol", unique: true, using: :btree
 
+<<<<<<< HEAD
   create_table "sentiment_caches", force: :cascade do |t|
     t.datetime "tweet_when"
     t.decimal  "score",                    precision: 6, scale: 3
@@ -36,6 +37,18 @@ ActiveRecord::Schema.define(version: 20151030213319) do
   end
 
   add_index "sentiment_caches", ["company_id"], name: "index_sentiment_caches_on_company_id", unique: true, using: :btree
+=======
+  create_table "finance_caches", force: :cascade do |t|
+    t.text     "hist_data",  limit: 65535
+    t.text     "curr_data",  limit: 65535
+    t.integer  "status",     limit: 4,     default: 0
+    t.integer  "company_id", limit: 4
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
+
+  add_index "finance_caches", ["company_id"], name: "index_finance_caches_on_company_id", using: :btree
+>>>>>>> Added caching of historical and current data
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -55,5 +68,9 @@ ActiveRecord::Schema.define(version: 20151030213319) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+<<<<<<< HEAD
   add_foreign_key "sentiment_caches", "companies"
+=======
+  add_foreign_key "finance_caches", "companies"
+>>>>>>> Added caching of historical and current data
 end
