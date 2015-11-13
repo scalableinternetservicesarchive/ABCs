c9a0f478e426c430611b91858dd828bd13796e5e
Sakibs <sakib@wifi-131-179-38-62.host.ucla.edu>
2015-11-12 16:08:22 -0800

    Initial dashboard design implemented

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,46 @@
+require 'yahoo_finance'
+require 'sentiment_analyzer'
+
+class HomeController < ApplicationController
+  def dashboard
+    return unless current_user
+    # get IDs of companies that are currently favorited
+    user_favorite_active = current_user.favorite_companys.where(active: true).map(&:company_id)
+    # Set the current user companies to the active ones
+    @user_companies = current_user.companys.where(id: user_favorite_active)
+
+    @tickers = ticker_json(Company.all)
+
+    return unless params['symbol']
+    @symbol = params['symbol'].upcase
+    # @search_results = Company.where("symbol like :prefix", prefix: "%#{@symbol}%")
+    @search_results = Company.find_by_symbol(@symbol)
+
+    puts "===================="
+    if @search_results
+      puts @search_results
+      @quotes = Finance.get_quotes(@symbol, false)
+      @hist = Finance.get_quotes(@symbol, true)
+
+      @sentiments = SentimentAnalyzer.get_results(@symbol)
+      @symbol_rating = @sentiments[:symbol_rating]
+      @num_tweets = @sentiments[:num_tweets]
+      @results = @sentiments[:results]
+    else
+
+    end
+    puts "===================="
+  rescue => e
+    puts e.message
+    render file: 'public/404.html', status: :not_found, layout: false
+  end
+
+  def ticker_json(companies)
+    list = companies.map(&:symbol)
+    list.to_json
+  end
+
+  def home_params
+    params.require(:symbol)
+  end
+end
