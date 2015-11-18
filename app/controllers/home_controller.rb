237f7fb23c6a9a7562020fe35d5d9e888b99de32
Sakibs <sakib@wifi-131-179-38-250.host.ucla.edu>
2015-11-17 18:49:03 -0800

    Fix dashboard to work for guests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,6 @@ require 'sentiment_analyzer'
 
 class HomeController < ApplicationController
   def dashboard
-    return unless current_user
-    # get IDs of companies that are currently favorited
-    user_favorite_active = current_user.favorite_companys.where(active: true).map(&:company_id)
-    # Set the current user companies to the active ones
-    @user_companies = current_user.companys.where(id: user_favorite_active)
-
     @tickers = ticker_json(Company.all)
 
     # return unless params['symbol']
@@ -27,7 +21,13 @@ class HomeController < ApplicationController
       else
         @search_results = Company.where("symbol like :prefix", prefix: "%#{@symbol}%")
       end
+
+      return unless current_user
+      @user_companies = get_user_companies()
     else
+      return unless current_user
+      @user_companies = get_user_companies()
+
       @user_companies_quotes = Array.new
       @user_companies.each do |c|
         quote = Finance.get_quotes(c.symbol, false)
@@ -37,6 +37,13 @@ class HomeController < ApplicationController
   rescue => e
     puts e.message
     render file: 'public/404.html', status: :not_found, layout: false
+  end
+
+  def get_user_companies()
+    # get IDs of companies that are currently favorited
+    user_favorite_active = current_user.favorite_companys.where(active: true).map(&:company_id)
+    # Set the current user companies to the active ones
+    return current_user.companys.where(id: user_favorite_active)
   end
 
   def ticker_json(companies)
